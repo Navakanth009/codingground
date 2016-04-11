cd3e2842f013dd17dd4839a0cde4dcf5f0d69df1
Navakanth009 <navakanth09@gmail.com>
2016-04-11 18:19:46 +0530

    Created by Coding Ground Client

---
+++ VaImageContrasts/demo.m
@@ -0,0 +1,3 @@
+%imread('https://s3.amazonaws.com/GPImages/cc9cbf498dc8ecf91445576021150.jpg')
+disp('******************************************************************************************')
+va_image_contrasts('https://s3.amazonaws.com/GPImages/cc9cbf498dc8ecf91445576021150.jpg')
